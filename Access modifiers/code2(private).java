3a33d7e0795be94ab3fa40fa17483f0290519409
Vinayakumara S S <vinaykumarss904@gmail.com>
2023-11-24 19:08:54 +0530

    Added new file

---
+++ Access modifiers/code2(private).java
@@ -0,0 +1,17 @@
+package code8;
+class naming
+{
+	private String name="vinay";
+	public void printname()
+	{
+		System.out.println(name);
+	}
+}
+
+public class example {
+	public static void main(String[] args) {
+		naming n=new naming();
+		n.printname();
+	}
+
+}
