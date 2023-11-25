da75f78e2bc9b5a6f2344b374efe6e70c90b8d7f
Vinayakumara S S <vinaykumarss904@gmail.com>
2023-11-25 15:14:52 +0530

    Added new file

---
+++ Constructor/code1.java
@@ -0,0 +1,16 @@
+package code9;
+class employee
+{
+	public employee()
+	{
+		System.out.println("hello world");
+	}
+}
+public class example 
+{
+	public static void main(String[] args)
+	{
+		employee e=new employee();
+	}
+	
+}
