06050905dde5c72e836da5bc980a01bd3da68e30
Vinayakumara S S <vinaykumarss904@gmail.com>
2023-12-11 20:57:25 +0530

    Added new file

---
+++ Collections/arraylist3.java
@@ -0,0 +1,11 @@
+package collections;
+import java.util.*;
+public class arraylist3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> l1=  new ArrayList<>();
+        l1.add(10);
+        l1.add(20);
+        l1.add(4);
+        System.out.println(l1.contains(10));
+    }
+}
